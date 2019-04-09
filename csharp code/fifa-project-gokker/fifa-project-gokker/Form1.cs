568adc9685aeff6b19d4ac8cf66ed0b000a8fe97
Unknown <mennovermeulen1@gmail.com>
2019-04-09 12:05:11 +0200

    menu toegevoegd
    menu toegevoegd

--- csharp code/fifa-project-gokker/fifa-project-gokker/Form1.cs
+++ csharp code/fifa-project-gokker/fifa-project-gokker/Form1.cs
@@ -16,5 +16,10 @@ namespace fifa_project_gokker
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
